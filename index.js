1e03c2f36f9fba9391ea293c51a625d91e9ea8f1
Sanjib Narzary <alayaran@gmail.com>
2016-09-30 01:40:26 +0530

    Comment added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var log = console.log;
-var Caesar = require('./library/caesar.js')
+var Caesar = require('./library/caesar.js');
 var c = new Caesar();
 var o = c.encrypt('Hello',5);
 var d =c.decrypt(o,5);
-log(o)
-log(d)
+log(o);
+log(d);
--- library/caesar.js
+++ library/caesar.js
@@ -1,14 +1,32 @@
+/**
+* Program: Caesar Cipher Nodejs Library module
+* Name: Sanjib Narzary
+* Email: <san@cit.ac.in>
+* Organization: CIT Kokrajhar
+* Website: http://www.cit.ac.in/CSE
+*
+**/
+
+// This Caesar class has only one property that is array of aplhabets and inspect
+// it has two prototype function encrypt and decrypy
+// This is a Caesar class
 var Caesar = function(){
-  // this.str = str;
-  // this.key = key;
+  // Lets create the allowed characters for Caesar cipher. Here
+  // I have created 27 character array as it was only 26 in the
+  // book. I have added one more character to support for space :)
   this.alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
                   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' '];
 }
+// The function for encrypting message. This is a function which takes
+// two arguments first parameter is the plaintext to be encrypted and the second
+// parameter is the secret key. this function will return the encrypted message
 
 Caesar.prototype.encrypt = function(text,key){
-  var str = text.toUpperCase();
-  //log(str);
+  // we dont support lowercase so lets make upper case
+  // you may implement yourself to support both lower case and upper case
 
+  var str = text.toUpperCase();
+  //initialize some variables
   var m=0, n = 0, res = "";
   for(i=0;i<str.length;i++){
     m = this.alphabets.indexOf(str.charAt(i));
@@ -17,11 +35,15 @@ Caesar.prototype.encrypt = function(text,key){
   }
   return res;
 }
+//The function for decrypting any encrypted message which are encrypted
+//with caesar cipher. This function accepts two arguments, first argument
+//is the encrypted message and the second parameter is the secret key
+//After decrypting this function will return original plaintext message
 
 Caesar.prototype.decrypt = function(text,key){
+  //Making the upper cases
   var str = text.toUpperCase();
-  //log(str);
-
+  //initialize variables to be used in the for loops
   var m=0, n = 0, res = ""
   for(i=0;i<str.length;i++){
     m = this.alphabets.indexOf(str.charAt(i));
@@ -31,4 +53,5 @@ Caesar.prototype.decrypt = function(text,key){
   return res;
 }
 
+//export the Caesar class as module so that we can import it using required or import
 module.exports = Caesar;
